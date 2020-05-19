316149016d580543c6c0333c3ab106c834fdb2df
Amir <a.h.hadian@gmail.com>
2020-05-19 14:07:56 +0430

    updated regex

--- LexicalAnalyser/tokens.py
+++ LexicalAnalyser/tokens.py
@@ -11,36 +11,101 @@ tokens = [
     },
     {
         'token': 'T_INTEGERLITERAL',
-        'pattern': '[0-9]+'
+        'pattern': '\d+'
+    },
+    {
+        'token': 'T_DOUBLELITERAL',
+        'pattern': '\d+\.\d*([eE][+-]?\d+)?'
+    },
+    {
+        'token': 'T_ID',
+        'pattern': '[a-z|A-Z]{0,30}\w'
+    },
+    # key words
+    {
+        'token': 'T_VOID',
+        'pattern': 'void'
+    },
+    {
+        'token': 'T_INT',
+        'pattern': 'int'
+    },
+    {
+        'token': 'T_BOOL',
+        'pattern': 'bool'
+    },
+    {
+        'token': 'T_DOUBLE',
+        'pattern': 'double'
+    },
+    {
+        'token': 'T_STRING',
+        'pattern': 'string'
     },
     {
         'token': 'T_CLASS',
         'pattern': 'class'
     },
     {
-        'token': 'T_IF',
-        'pattern': 'if'
+        'token': 'T_INTERFACE',
+        'pattern': 'interface'
+    },
+    {
+        'token': 'T_NULL',
+        'pattern': 'null'
+    },
+    {
+        'token': 'T_EXTENDS',
+        'pattern': 'extends'
+    },
+    {
+        'token': 'T_IMPLEMENTS',
+        'pattern': 'implements'
+    },
+    {
+        'token': 'T_FOR',
+        'pattern': 'for'
     },
     {
         'token': 'I_WHILE',
         'pattern': 'while'
     },
     {
-        'token': 'T_{',
-        'pattern': '{'
+        'token': 'T_IF',
+        'pattern': 'if'
+    }, {
+        'token': 'I_ELSE',
+        'pattern': 'else'
     },
     {
-        'token': 'T_}',
-        'pattern': '}'
+        'token': 'I_RETURN',
+        'pattern': 'return'
     },
     {
-        'token': 'T_)',
-        'pattern': '[)]'
+        'token': 'I_BREAK',
+        'pattern': 'break'
     },
     {
-        'token': 'T_(',
-        'pattern': '[(]'
+        'token': 'I_NEW',
+        'pattern': 'new'
     },
+    {
+        'token': 'I_NEWARRAY',
+        'pattern': 'NewArray'
+    },
+    {
+        'token': 'I_PRINT',
+        'pattern': 'print'
+    },
+    {
+        'token': 'I_READINTEGER',
+        'pattern': 'ReadInteger'
+    },
+    {
+        'token': 'I_READLINE',
+        'pattern': 'ReadLine'
+    },
+    # operands and punctuations
     {
         'token': 'T_+',
         'pattern': '\+'
@@ -58,15 +123,83 @@ tokens = [
         'pattern': '/'
     },
     {
-        'token': 'T_;',
-        'pattern': ';'
+        'token': 'T_%',
+        'pattern': '%'
+    },
+    {
+        'token': 'T_<',
+        'pattern': '<'
+    },
+    {
+        'token': 'T_<=',
+        'pattern': '<='
+    },
+    {
+        'token': 'T_>',
+        'pattern': '>'
+    },
+    {
+        'token': 'T_>=',
+        'pattern': '>='
+    },
+    {
+        'token': 'T_=',
+        'pattern': '='
     },
     {
         'token': 'T_==',
         'pattern': '=='
     },
     {
-        'token': 'T_ID',
-        'pattern': '[a-z|A-Z][0-9|a-z|A-Z]*'
+        'token': 'T_!=',
+        'pattern': '!='
     },
+    {
+        'token': 'T_&&',
+        'pattern': '&&'
+    },
+    {
+        'token': 'T_||',
+        'pattern': '\|\|'
+    },
+    {
+        'token': 'T_!',
+        'pattern': '!'
+    },
+    {
+        'token': 'T_;',
+        'pattern': ';'
+    },
+    {
+        'token': 'T_,',
+        'pattern': ','
+    },
+    {
+        'token': 'T_.',
+        'pattern': '\.'
+    },
+    {
+        'token': 'T_{',
+        'pattern': '\{'
+    },
+    {
+        'token': 'T_}',
+        'pattern': '\}'
+    },
+    {
+        'token': 'T_)',
+        'pattern': '\)'
+    },
+    {
+        'token': 'T_(',
+        'pattern': '\('
+    },
+    {
+        'token': 'T_]',
+        'pattern': '\]'
+    },
+    {
+        'token': 'T_[',
+        'pattern': '\['
+    }
 ]
