0d29042fba58f6dc43635617692c8c6c049bb75b
Amir <a.h.hadian@gmail.com>
2020-06-17 22:00:34 +0430

    grammar updated

--- SyntaxAnalyser/grammar.py
+++ SyntaxAnalyser/grammar.py
@@ -15,13 +15,28 @@ grammar = '''
     stmt_block : T_ROUND_BRACKET_OPEN variable_decl* stmt* T_ROUND_BRACKET_CLOSE
     stmt : T_SMALLER expr T_BIGGER T_SEMICOLON | i_f_stmt | while_stmt | for_stmt | break_stmt | return_stmt | print_stmt | stmt_block
     i_f_stmt : T_IF T_ROUND_BRACKET_OPEN expr T_ROUND_BRACKET_CLOSE
-    while_stmt : I_WHILE T_PARENTHESES_OPEN expr T_PARENTHESES_CLOSE stmt
-    for_stmt = T_FOR T_PARENTHESES_OPEN T_SMALLER expr T_BIGGER T_SEMICOLON T_SMALLER expr T_BIGGER T_SEMICOLON T_SMALLER expr T_BIGGER T_PARENTHESES_CLOSE stmt
-    return_stmt = I_RETURN T_SMALLER expr T_BIGGER T_SEMICOLON
-    break_stmt : I_BREAK T_SEMICOLON
-    print_stmt : I_PRINT T_PARENTHESES_OPEN expr (T_COMMA expr)* T_PARENTHESES_CLOSE T_SEMICOLON
+    while_stmt : T_WHILE T_PARENTHESES_OPEN expr T_PARENTHESES_CLOSE stmt
+    for_stmt : T_FOR T_PARENTHESES_OPEN T_SMALLER expr T_BIGGER T_SEMICOLON expr T_SEMICOLON T_SMALLER expr T_BIGGER T_PARENTHESES_CLOSE stmt
+    return_stmt : T_RETURN T_SMALLER expr T_BIGGER T_SEMICOLON
+    break_stmt : T_BREAK T_SEMICOLON
+    print_stmt : T_PRINT T_PARENTHESES_OPEN expr (T_COMMA expr)* T_PARENTHESES_CLOSE T_SEMICOLON
+    expr : l_value T_EQUAL expr | j
+    j : j T_OR i | i
+    i : i T_AND h | h
+    h : h T_EQUAL_EQUAL g | h T_NOT_EQUAL g | g
+    g : g T_SMALLER_EQUAL f | g T_BIGGER_EQUAL f | g T_SMALLER f| g T_BIGGER f | f
+    f : f T_PLUS e | f T_MINUS e | e
+    e : e T_MULT d | e T_DIVIDE d | e T_PERCENTAGE d | d
+    d : T_NOT d | c
+    c : T_MINUS c | b
+    b : T_PARENTHESES_OPEN b T_PARENTHESES_CLOSE | a
+    a : constant | l_value | T_THIS | call | T_READINTEGER T_PARENTHESES_OPEN T_PARENTHESES_CLOSE
+    | T_READLINE T_PARENTHESES_OPEN T_PARENTHESES_CLOSE | T_NEW T_ID | T_NEWARRAY T_PARENTHESES_OPEN expr T_COMMA type T_PARENTHESES_CLOSE
+    l_value : T_ID | expr T_DOT T_ID | expr T_BRACKET_OPEN expr T_BRACKET_CLOSE
+    call : T_ID T_PARENTHESES_OPEN actuals T_PARENTHESES_CLOSE | expr T_DOT T_ID T_PARENTHESES_OPEN actuals T_PARENTHESES_CLOSE
+    actuals : expr (T_COMMA expr)* |
+    constant : T_INTLITERAL | T_DOUBLELITERAL | T_BOOLEANLITERAL | T_STRINGLITERAL | T_NULL
     
-    expr : 
     //terminals
     
     //keywords
@@ -37,16 +52,16 @@ grammar = '''
     T_EXTENDS : "extends"
     T_IMPLEMENTS : "implements"
     T_FOR : "for"
-    I_WHILE : "while"
+    T_WHILE : "while"
     T_IF : "if"
-    I_ELSE : "else"
-    I_RETURN : "return"
-    I_BREAK : "break"
-    I_NEW : "new"
-    I_NEWARRAY : "NewArray"
-    I_PRINT : "Print"
-    I_READINTEGER : "ReadInteger"
-    I_READLINE : "ReadLine"
+    T_ELSE : "else"
+    T_RETURN : "return"
+    T_BREAK : "break"
+    T_NEW : "new"
+    T_NEWARRAY : "NewArray"
+    T_PRINT : "Print"
+    T_READINTEGER : "ReadInteger"
+    T_READLINE : "ReadLine"
     
     //literals
     T_BOOLEANLITERAL : /(false|true)/
@@ -56,8 +71,8 @@ grammar = '''
     T_ID : /[a-z|A-Z]\w{0,30}/
     
     //comments
-    T_INLINE_COMMENT : /\/\/.*\n/
-    T_COMMENT : /\/\*(.*\n*)*\*\//
+    INLINE_COMMENT : "//" /[^\\n]*/ "\\n"
+    COMMENT : "/*" /[^\\n]*/ "*/"
     
     //punctuations
     T_PLUS : "+"
@@ -65,13 +80,13 @@ grammar = '''
     T_MULT : "*"
     T_DIVIDE : "/"
     T_PERCENTAGE : "%"
-    T_SMALLER : "<"
     T_SMALLER_EQUAL : "<="
-    T_BIGGER : ">"
     T_BIGGER_EQUAL : ">="
+    T_SMALLER : "<"
+    T_BIGGER : ">"
     T_EQUAL_EQUAL : "=="
-    T_EQUAL : "="
     T_NOT_EQUAL : "!="
+    T_EQUAL : "="
     T_AND : "&&"
     T_OR : "||"
     T_NOT : "!"
