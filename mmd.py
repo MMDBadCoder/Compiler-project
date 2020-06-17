833b3bc9186a21dcf5b74bcca7fab5c72628cc83
MMDBadCoder <gitmmdgit@gmail.com>
2020-06-17 22:55:08 +0430

    mmd

--- mmd.py
+++ mmd.py
@@ -2,6 +2,10 @@ regex1 = '^/\*' + '([^\*]|\*[^/])*' + '\*/$'
 
 import re
 
-text = '/* /* hellow sclsdcsdc * *csd sdcskdc */'
+text = '''/* /* hellow sclsdcsdc * *
+sdc
+sdcsd
+csdc
+sdccsd sdcskdc */'''
 obj = re.search(regex1, text)
 print(obj)
