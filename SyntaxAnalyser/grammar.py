1162a6fd50b1799f69ba530323d98d300f3ca674
Amir <a.h.hadian@gmail.com>
2020-06-17 23:24:16 +0430

    <> problem

--- SyntaxAnalyser/grammar.py
+++ SyntaxAnalyser/grammar.py
@@ -8,16 +8,16 @@ grammar = '''
     type: T_INT | T_BOOL | T_DOUBLE | T_STRING | T_ID | type T_BRACKET_OPEN T_BRACKET_CLOSE
     function_decl : type T_ID T_PARENTHESES_OPEN formals T_PARENTHESES_CLOSE stmt_block | T_VOID T_ID T_PARENTHESES_OPEN formals T_PARENTHESES_CLOSE stmt_block
     formals : variable (T_COMMA variable)* |
-    class_decl : T_CLASS T_ID T_SMALLER T_EXTENDS T_ID T_BIGGER T_SMALLER T_IMPLEMENTS T_ID (T_COMMA T_ID)* T_BIGGER T_ROUND_BRACKET_OPEN field* T_ROUND_BRACKET_CLOSE
+    class_decl : T_CLASS T_ID (T_EXTENDS T_ID)? (T_IMPLEMENTS T_ID (T_COMMA T_ID)*)? T_ROUND_BRACKET_OPEN field* T_ROUND_BRACKET_CLOSE
     field : variable_decl | function_decl
     interface_decl : T_INTERFACE T_ID T_ROUND_BRACKET_OPEN prototype* T_ROUND_BRACKET_CLOSE
     prototype : type T_ID T_PARENTHESES_OPEN formals T_PARENTHESES_CLOSE T_SEMICOLON | T_VOID T_ID T_PARENTHESES_OPEN formals T_PARENTHESES_CLOSE T_SEMICOLON
     stmt_block : T_ROUND_BRACKET_OPEN variable_decl* stmt* T_ROUND_BRACKET_CLOSE
-    stmt : T_SMALLER expr T_BIGGER T_SEMICOLON | i_f_stmt | while_stmt | for_stmt | break_stmt | return_stmt | print_stmt | stmt_block
+    stmt : (expr)? T_SEMICOLON | i_f_stmt | while_stmt | for_stmt | break_stmt | return_stmt | print_stmt | stmt_block
     i_f_stmt : T_IF T_ROUND_BRACKET_OPEN expr T_ROUND_BRACKET_CLOSE
     while_stmt : T_WHILE T_PARENTHESES_OPEN expr T_PARENTHESES_CLOSE stmt
-    for_stmt : T_FOR T_PARENTHESES_OPEN T_SMALLER expr T_BIGGER T_SEMICOLON expr T_SEMICOLON T_SMALLER expr T_BIGGER T_PARENTHESES_CLOSE stmt
-    return_stmt : T_RETURN T_SMALLER expr T_BIGGER T_SEMICOLON
+    for_stmt : T_FOR T_PARENTHESES_OPEN (expr)? T_SEMICOLON expr T_SEMICOLON (expr)? T_PARENTHESES_CLOSE stmt
+    return_stmt : T_RETURN expr? T_SEMICOLON
     break_stmt : T_BREAK T_SEMICOLON
     print_stmt : T_PRINT T_PARENTHESES_OPEN expr (T_COMMA expr)* T_PARENTHESES_CLOSE T_SEMICOLON
     expr : expr T_EQUAL expr | expr T_DOT T_ID | expr T_BRACKET_OPEN expr T_BRACKET_CLOSE | j
