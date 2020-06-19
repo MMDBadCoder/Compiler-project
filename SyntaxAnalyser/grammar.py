562a644a5a3202276fef7e58fd10326648112962
Amir <a.h.hadian@gmail.com>
2020-06-19 19:59:14 +0430

    grammar

--- SyntaxAnalyser/grammar.py
+++ SyntaxAnalyser/grammar.py
@@ -1,11 +1,11 @@
 grammar = '''
     //main grammar
-    
+
     start : decl+
     decl : variable_decl | function_decl | class_decl | interface_decl
     variable_decl : variable T_SEMICOLON
     variable : type T_ID
-    type: T_INT | T_BOOL | T_DOUBLE | T_STRING | T_ID | type T_BRACKET_OPEN T_BRACKET_CLOSE
+    type: T_INT | T_BOOL | T_DOUBLE | T_STRING | T_ID 
     function_decl : type T_ID T_PARENTHESES_OPEN formals T_PARENTHESES_CLOSE stmt_block | T_VOID T_ID T_PARENTHESES_OPEN formals T_PARENTHESES_CLOSE stmt_block
     formals : variable (T_COMMA variable)* |
     class_decl : T_CLASS T_ID (T_EXTENDS T_ID)? (T_IMPLEMENTS T_ID (T_COMMA T_ID)*)? T_ROUND_BRACKET_OPEN field* T_ROUND_BRACKET_CLOSE
@@ -20,25 +20,25 @@ grammar = '''
     return_stmt : T_RETURN expr? T_SEMICOLON
     break_stmt : T_BREAK T_SEMICOLON
     print_stmt : T_PRINT T_PARENTHESES_OPEN expr (T_COMMA expr)* T_PARENTHESES_CLOSE T_SEMICOLON
-    expr : expr T_EQUAL expr | expr T_DOT T_ID | expr T_BRACKET_OPEN expr T_BRACKET_CLOSE | j
-    j : j T_OR i | i
-    i : i T_AND h | h
-    h : h T_EQUAL_EQUAL g | h T_NOT_EQUAL g | g
-    g : g T_SMALLER_EQUAL f | g T_BIGGER_EQUAL f | g T_SMALLER f| g T_BIGGER f | f
-    f : f T_PLUS e | f T_MINUS e | e
-    e : e T_MULT d | e T_DIVIDE d | e T_PERCENTAGE d | d
-    d : T_NOT d | c
-    c : T_MINUS c | k
-    k : k T_DOT T_ID T_PARENTHESES_OPEN actuals T_PARENTHESES_CLOSE | b
-    b : T_PARENTHESES_OPEN b T_PARENTHESES_CLOSE | a
-    a : constant | T_ID | T_THIS | call | T_READINTEGER T_PARENTHESES_OPEN T_PARENTHESES_CLOSE
-    | T_READLINE T_PARENTHESES_OPEN T_PARENTHESES_CLOSE | T_NEW T_ID
-    call : T_ID T_PARENTHESES_OPEN actuals T_PARENTHESES_CLOSE
+    expr : i T_EQUAL expr | a
+    a : a T_OR b | b
+    b : b T_AND c | c
+    c : c T_EQUAL_EQUAL d | c T_NOT_EQUAL d | d
+    d : d T_SMALLER e | d T_SMALLER_EQUAL e | d T_BIGGER e | d T_BIGGER_EQUAL e | e
+    e : e T_PLUS f | e T_MINUS f | f
+    f : f T_MULT g | f T_DIVIDE g | f T_PERCENTAGE g | g
+    g : T_NOT g | T_MINUS g | i
+    i : i T_DOT T_ID | h T_BRACKET_OPEN expr T_BRACKET_CLOSE | i T_DOT T_ID T_PARENTHESES_OPEN actuals T_PARENTHESES_CLOSE | h
+    h : T_PARENTHESES_OPEN expr T_PARENTHESES_CLOSE | atomic
+    atomic : constant | T_THIS | call | T_READLINE T_PARENTHESES_OPEN T_PARENTHESES_CLOSE 
+    | T_READINTEGER  T_PARENTHESES_OPEN T_PARENTHESES_CLOSE| T_NEW T_ID 
+    | T_NEWARRAY T_PARENTHESES_OPEN expr T_COMMA type T_PARENTHESES_CLOSE | T_ID
+    call : T_ID T_PARENTHESES_OPEN actuals T_PARENTHESES_CLOSE 
     actuals : expr (T_COMMA expr)* |
     constant : T_INTLITERAL | T_DOUBLELITERAL | T_BOOLEANLITERAL | T_STRINGLITERAL | T_NULL
-    
+
     //terminals
-    
+
     //keywords
     T_VOID : "void"
     T_INT : "int"
@@ -62,18 +62,18 @@ grammar = '''
     T_PRINT : "Print"
     T_READINTEGER : "ReadInteger"
     T_READLINE : "ReadLine"
-    
+
     //literals
     T_BOOLEANLITERAL : /(false|true)/
     T_STRINGLITERAL : /".*"/
     T_INTLITERAL : /((0[xX](\d|[a-f|A-F])+)|\d+)/
     T_DOUBLELITERAL : /\d+\.\d*([eE][+-]?\d+)?/
     T_ID : /[a-z|A-Z]\w{0,30}/
-    
+
     //comments
     INLINE_COMMENT : "//" /[^\\n]*/ "\\n"
     COMMENT : "/*" /[^\\n]*/ "*/"
-    
+
     //punctuations
     T_PLUS : "+"
     T_MINUS : "-"
@@ -100,7 +100,7 @@ grammar = '''
     T_BRACKET_CLOSE : "]"
     T_BRACKET_OPEN : "["
     T_COLON : ":"
-    
+
     //ignores
     %import common.WS
     %ignore WS
