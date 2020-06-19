f844624f0e37e616d64a4146427a936be2dff3bc
Amir <a.h.hadian@gmail.com>
2020-06-19 22:48:52 +0430

    phase 2 cmplt

--- SyntaxAnalyser/parser.py
+++ SyntaxAnalyser/parser.py
@@ -1,8 +1,9 @@
-def parse(input):
+def parse(input_file):
+    input_str = input_file.read()
     from SyntaxAnalyser.lark import parser
     error = False
     try:
-        parser.parse(input)
+        parser.parse(input_str)
     except Exception as e:
         error = True
     return not error
